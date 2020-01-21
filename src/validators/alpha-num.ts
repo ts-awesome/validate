2f8da6513c48897df9bd0337bc3038b783945c48
Volodymyr Iatsyshyn <viatsyshyn@gmail.com>
2020-01-21 17:18:54 +0200

    updated alpha-num validator

--- src/validators/alpha-num.ts
+++ src/validators/alpha-num.ts
@@ -1,15 +1,23 @@
 import {validators} from 'validate.js';
 import {ValidatorFunction, ValidatorInstance, ValidatorOptions} from "../interfaces";
 
+export interface AlphaNumProps {
+  allowSpaces?: boolean
+}
+
 validators.alpha_num = ((value, options) => {
-  if (value?.match(/[!@#$%^&*(),.?":{}|<>\s]/)) {
+  if (value?.match(/[!@#$%^&*(),.?":{}|<>]/)) {
     return options.message ?? `should be alphanumeric`;
   }
-  return;
+  if (options.allowSpaces === false && /\s/.test(value)) {
+    return options.message ?? `should be alphanumeric`;
+  }
 }) as ValidatorFunction;
 
-export function alphaNumeric(options?: ValidatorOptions<{}>): ValidatorInstance<'alpha_num'> {
-  return {alpha_num: options ?? true};
+export function alphaNumeric(allowSpaces: boolean): ValidatorInstance<'alpha_num'>;
+export function alphaNumeric(options?: ValidatorOptions<AlphaNumProps>): ValidatorInstance<'alpha_num'>;
+export function alphaNumeric(options?: ValidatorOptions<AlphaNumProps> | boolean): ValidatorInstance<'alpha_num'> {
+  return {alpha_num: typeof options === 'boolean' ? {allowSpaces: options} : options ?? true};
 }
 
 
