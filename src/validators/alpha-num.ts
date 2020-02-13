b604f66a6387c2a6a5b0b515cd65e35a7c4b7f39
Volodymyr Iatsyshyn <viatsyshyn@gmail.com>
2020-02-13 23:18:23 +0200

    fixed alphanum validator

--- src/validators/alpha-num.ts
+++ src/validators/alpha-num.ts
@@ -1,4 +1,4 @@
-import {validators} from 'validate.js';
+import {isDefined, validators} from 'validate.js';
 import {ValidatorFunction, ValidatorInstance, ValidatorOptions} from "../interfaces";
 
 export interface AlphaNumProps {
@@ -6,11 +6,21 @@ export interface AlphaNumProps {
 }
 
 validators.alpha_num = ((value, options) => {
-  if (value?.match(/[!@#$%^&*(),.?":{}|<>]/)) {
-    return options.message ?? `should be alphanumeric`;
+  // Empty values are allowed. Use presence validator along with this validator for required boolean property.
+  if (!isDefined(value)) {
+    return;
   }
+
+  if (typeof value !== 'string') {
+    return options.message ?? "must be string";
+  }
+
+  if (value?.match(/[!@#$%^&*(),.?":{}|<>]/)) {
+    return options.message ?? `must be alphanumeric`;
+  }
+
   if (options.allowSpaces === false && /\s/.test(value)) {
-    return options.message ?? `should be alphanumeric`;
+    return options.message ?? `must be alphanumeric without spaces`;
   }
 }) as ValidatorFunction;
 
