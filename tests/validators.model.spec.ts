970555f413160a361929afe02f9dd7d2a9e7c33f
Volodymyr Iatsyshyn <viatsyshyn@gmail.com>
2021-08-20 12:09:54 +0300

    more tests

--- tests/validators.model.spec.ts
+++ tests/validators.model.spec.ts
@@ -1,10 +1,13 @@
 import {model} from "../src/validators/model";
-import {presence, type} from "../src/validators";
+import {array, presence, type} from "../src/validators";
 import {validate} from "../src";
 
 class Model {
   @validate([presence(), type('string')])
-  public test!: unknown
+  public test!: string;
+
+  @validate([array({element: [model(Model)]})])
+  public leafs?: Model[];
 }
 
 describe('validators.model', function () {
@@ -17,10 +20,16 @@ describe('validators.model', function () {
     expect(model(Model)({test: 'ok'}, 'key', {}, {})).not.toBeDefined();
   });
   it('check empty model', () => {
-    expect(model(Model)({}, 'key', {}, {})).toEqual('must be an object');
+    expect(model(Model)({}, 'key', {}, {})).toEqual(['Test can\'t be blank']);
   });
   it('check invalid model', () => {
-    expect(model(Model)({test: false}, 'key', {}, {})).toEqual('must be an object');
+    expect(model(Model)({test: false}, 'key', {}, {})).toEqual(['Test must be of the correct type string']);
+  });
+  it('check nested', () => {
+    expect(model(Model)({test: 'ok', leafs: [{test:'ok'}]}, 'key', {}, {})).toEqual(undefined);
+  });
+  it('check nested fails', () => {
+    expect(model(Model)({test: 'ok', leafs: [{test:false}]}, 'key', {}, {})).toEqual(['Leafs [0] Test must be of the correct type string']);
   });
 
 })
